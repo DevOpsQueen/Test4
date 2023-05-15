0225207967e8c5e585a18190a945a2381e8197ba
DevOpsQueen <110736912+DevOpsQueen@users.noreply.github.com>
2023-05-15 14:39:07 -0400

    Update deploy.sh
    Updates after debug and successful deploy confirmation

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Start Tomcat with the specified credentials
-/usr/local/tomcat/bin/startup.sh && \
-curl --retry 10 --retry-delay 5 --retry-connrefused http://localhost:8080 && \
+/usr/local/tomcat/bin/catalina.sh start
 
 # Wait for Tomcat to fully start
-sleep 10 && \
+sleep 10
 
 # Deploy the WAR file
 curl --retry 10 --retry-delay 5 --retry-connrefused -u $TOMCAT_USER:$TOMCAT_PASSWORD -X PUT \
   -H "Content-Type: application/octet-stream" \
   --data-binary @/usr/local/tomcat/webapps/$WAR_FILE \
-  http://localhost:8888/manager/text/deploy?path=/$WAR_FILE && \
+  http://localhost:8080/manager/text/deploy?path=/$WAR_FILE
 
 # Tail the Tomcat log
 tail -f /usr/local/tomcat/logs/catalina.out
